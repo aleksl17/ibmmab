c082007786d6aabf208b94ee131892954f0db9cd
Aleksander Markus Lingstad <aleksl17@uia.no>
2020-03-11 23:14:25 +0100

    fixed header typo

--- backend/app.js
+++ backend/app.js
@@ -119,8 +119,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
