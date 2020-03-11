74db67d550a78d745a59240941fc4584e16cf8fa
Thor Eivind Forberg <thoref16@student.uia.no>
2020-03-11 13:30:27 +0100

    backup

---
+++ frontend/src/components/Main_Page.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <div class="Main" id="main">
+        <h1>{{msg}}</h1>
+        <p id="p_1">{{info}}</p>
+
+
+    </div>
+</template>
+
+
+<script>
+    import * as axios from "core-js";
+
+    export default {
+        name: "Main_Page",
+        props: {
+            msg: String,
+        },
+        data()  {
+
+            return {
+                Greeting: focus(),
+                Response: axios.response(),
+            }
+
+        },
+        mounted () {
+            axios
+                .get('http://localhost:3000/api/anData')
+                .then(response => (this.info = response))
+        }
+    }
+    function focus()
+    {
+       //var MongoClient = require("mongodb").MongoClient;
+        var j=0;
+        var MongoClient = [1,2,3,4,5,6,7,8];
+        var y = "Results";
+
+      for (j = 0; j < MongoClient.length; j++)
+      {
+          if(MongoClient[j] > 3)
+            y += "<li>" + MongoClient[j] + "</li>";
+      }
+        return y;
+    }
+
+</script>
+
+
+<style scoped>
+p   {
+
+    }
+
+h1  {
+    font-size: xx-large;
+    margin: 50px 0 0;
+    }
+
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,83 @@
+
+<template>
+    <div class="topnav" id="topnavbar">
+        <btn class="topnavbtn" onclick="window.location=#main">
+        <span>Home</span>
+        </btn>
+        <btn class="topnavbtn" onclick="window.location='https://www.ibm.com/no-en';">
+        <span>IBM</span>
+        </btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Nav_Bar"
+    }
+
+
+
+
+</script>
+
+<style scoped>
+
+a
+{
+        color: white;
+
+
+    }
+
+.topnavbtn
+{
+    background: black;
+    height: 50px;
+    width: 25vw;
+    border-width: thin;
+    border: white;
+    border-style: double;
+    float: left;
+    color: white;
+    text-align: center;
+    line-height: 50px;
+
+
+}
+
+#topnavbar
+{
+
+        content: "";
+        display: table;
+        clear: both;
+    }
+
+.topnavbtn span
+{
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.5s;
+}
+
+.topnavbtn span:after {
+    content: '\00bb';
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: -20px;
+    transition: 0.5s;
+}
+
+.topnavbtn:hover span {
+    padding-right: 25px;
+}
+
+.topnavbtn:hover span:after {
+    opacity: 1;
+    right: 0;
+}
+
+
+</style>
