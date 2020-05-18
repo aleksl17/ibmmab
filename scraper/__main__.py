474f02a9fa04518cb0c18fe41b811865000dfb7d
Havard Friborg <haavfr17@uia.no>
2020-05-18 15:57:23 +0200

    fixed only 6 articles bug

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -18,20 +18,22 @@ def main():
     # print(len(enda))
     length = 0
     how_many = 0
+    print(len(enda))
 
     for item in enda:
-        # print(item.url)
+        #print(item.url)
         translate_article(item)
         if item.text == "NA":
             continue
         # print(item.text)
         # length += len(item.text)
         how_many += 1
-        # print(how_many)
+        print(how_many)
         try:
             author = item.authors[0]
         except Exception:
-            return "NA"
+            author = "NA"
+        #print(author)
         postable = {
             "text": item.text.replace("\n", " "),
             "title": item.title,
@@ -43,6 +45,8 @@ def main():
         }
         post = collection.insert_one(postable)
         print("Posted")
+
+    print(how_many)
 
 
 if __name__ == '__main__':
